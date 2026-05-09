2f73abbd3fa0f6e08aa1558fd4fe49535b4dc101
aajera <aajerabanu0508-github-email@gmail.com>
2026-05-09 21:54:32 +0530

    added backend api

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("AI Assisted DevOps Platform Backend Running");
+});
+
+app.get("/health", (req, res) => {
+    res.json({
+        status: "UP",
+        service: "backend",
+        timestamp: new Date()
+    });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
